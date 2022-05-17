55c1bcbf22ffabd6bce878d4d2a7f1ab445ec6a1
Shikiyo <shikiyo@archjk>
2022-05-16 21:57:55 -0300

    Premissas, PCB maior e mais partes do RoundRobin
    Mudança nas Premissas:
     * Tempos de IO alterados
     * Número de qios é constante
     * Novo tipo Placeholder para emular os dispositivos de IO
     * Mais dados no PCB:
        * tempos de inicio, fim e serviço
        * o próprio pid
        * (ppid removido)
    
    Novo no Round Robin:
     * Tempo da simulação (curr_time)
     * Fazendo loop (time_slice) e checando se
        o processo ainda está rodando
        (para cada unidade de tempo do time_slice)
     * Checando se há novos processo e criando,
        durante o loop do time_slice

--- main.c
+++ main.c
@@ -7,22 +7,8 @@
 #define __QUEUE_IMPL__
 #include "queue.h"
 
-/* espera algum processo chegar (CPU está ociosa):
- * retorna true (!= 0) se a simulação vai continuar,
- * retorna false (0) se a simulação terminou */
-b32 espera(Queue *qs, const u32 numq,
-        Queue *qios, const u32 numio,
-        PCB *pcbs, const u32 numpcb) {
-    (void) qs;   (void) numq;
-    (void) qios; (void) numio;
-    (void) pcbs; (void) numpcb;
-    // TODO: esperar o tempo passar,
-    // ou seja, até chegar alguém (novo ou bloqueado)
-    return 0;
-}
-
 typedef enum _Roda_Status {
-    Roda_Done = 0, Roda_Timeout, Roda_IO
+    Roda_Done = 0, Roda_NotDone, Roda_IO
 } Roda_Status;
 
 typedef struct _Roda_Ret {
@@ -30,26 +16,52 @@ typedef struct _Roda_Ret {
     IO_t io_type;
 } Roda_Ret;
 
-/* roda o processo:
- * retorna o que aconteceu com o processo:
+/* retorna o que aconteceu com o processo
+ * no tempo servido:
  *  * Terminou;
  *  * Recebeu timeout; ou
  *  * Vai fazer IO.
  */
-Roda_Ret roda(PID pid, PCB *pcb) {
-    (void) pid; (void) pcb;
+Roda_Ret roda(PID pid, time served) {
+    (void) pid; (void) served;
     Roda_Ret ret = {
-        .status = Roda_Done
+        .status = Roda_Done,
+        .io_type = IO_count,
     };
     return ret;
 }
 
-void robinfeedback(Queue *qs, const u32 numq,
-        Queue *qios, const u32 numio,
+/* espera algum processo chegar (CPU está ociosa):
+ * retorna true (!= 0) se a simulação vai continuar,
+ * retorna false (0) se a simulação terminou */
+b32 espera(Queue *qs, const u32 numq,
+        Queue *qios,
         PCB *pcbs, const u32 numpcb) {
-    assert( numq > 2 );
-    assert( numio > 2 );
+    (void) qs;   (void) numq;
+    (void) qios;
+    (void) pcbs; (void) numpcb;
+    // TODO: esperar o tempo passar,
+    // ou seja, até chegar alguém (novo ou bloqueado)
+    return 0;
+}
+
+/* verifica se algum processo chegou no tempo t.
+ * (similar a espera)
+ * retorna true (!= 0) se a simulação vai continuar,
+ * retorna false (0) se a simulação terminou
+ * retorna no ppid o novo pid */
+b32 new_process(time t, PID *pid) {
+    (void) t; (void) pid;
+    return 0;
+}
+
+void robinfeedback(Queue *qs, const u32 numq,
+        Queue *qios, IODev *iodevs,
+        PCB *pcbs, const u32 numpcb) {
+    assert( numq == 2 );
     assert( numpcb <= MAX_PID );
+
+    time curr_time = 0;
     b32 is_done = 1;
     while ( is_done ) {
         u32 first_not_empty = 0;
@@ -67,36 +79,82 @@ void robinfeedback(Queue *qs, const u32 numq,
             assert( pcbs[pid].priority == first_not_empty );
             pcbs[pid].status = p_running;
 
-            const Roda_Ret roda_ret = roda(pid, pcbs + pid);
+            time running = pcbs[pid].running_time;
+            time slice;
+            for ( slice = 0; slice < TIME_SLICE; slice++ ) {
+                if ( pcbs[pid].status != p_running )
+                    break;
 
-            // TODO: dar enqueue nos processos que chegaram (novos)
-            // TODO: dar enqueue nos processos que chegaram (IO)
+                const Roda_Ret roda_ret = roda(pid, running + slice);
 
-            switch ( roda_ret.status ) {
-                case Roda_Done: {
-                    pcbs[pid].status = p_done;
-                } break;
-                case Roda_Timeout: {
+                switch ( roda_ret.status ) {
+                    case Roda_Done: {
+                        pcbs[pid].status = p_done;
+                    } break;
+                    case Roda_NotDone: {
+                        // Nothing to do
+                    } break;
+                    case Roda_IO: {
+                        pcbs[pid].status = p_blocked;
+                        const IO_t io = roda_ret.io_type;
+                        assert( io < IO_count );
+                        pcbs[pid].priority = priority_from_io(io);
+                        const Queue_Err qerr =
+                            Queue_enqueue(qios + io, pid);
+                        assert( qerr == Queue_Ok );
+                    } break;
+                }
+
+                PID new_pid;
+                while ( new_process(curr_time, &new_pid) ) {
+                    assert( new_pid < numpcb );
+                    PCB new_pcb = {
+                        .status = p_ready,
+                        .pid = new_pid,
+                        .start_time = curr_time + slice,
+                        .end_time = 0,
+                        .running_time = 0,
+                        .priority = 0,
+                    };
+                    pcbs[new_pid] = new_pcb;
+                    const Queue_Err qerr =
+                        Queue_enqueue(qs + new_pid, pid);
+                    assert( qerr == Queue_Ok );
+                }
+                // TODO: dar enqueue nos processos que chegaram (IO)
+            }
+
+            pcbs[pid].running_time += slice;
+
+            switch ( pcbs[pid].status ) {
+                case p_done:
+                    pcbs[pid].end_time = curr_time + slice;
+                    break;
+                case p_running:
                     pcbs[pid].status = p_ready;
                     pcbs[pid].priority = numq - 1;
                     const Queue_Err qerr =
                         Queue_enqueue(qs + numq - 1, pid);
                     assert( qerr == Queue_Ok );
-                } break;
-                case Roda_IO: {
-                    pcbs[pid].status = p_blocked;
-                    const IO_t io = roda_ret.io_type;
-                    assert( io < numio );
-                    pcbs[pid].priority = priority_from_io(io, numio);
-                    const Queue_Err qerr =
-                        Queue_enqueue(qios + io, pid);
-                    assert( qerr == Queue_Ok );
-                } break;
-
+                    break;
+                case p_ready:
+                    assert( 0 && "unreachable" );
+                    break;
+                case p_blocked:
+                    // Nothing to do
+                    break;
             }
+
+            curr_time += slice;
         } else {
             // Não temos ninguém para rodar
-            is_done = espera(qs, numq, qios, numio, pcbs, numpcb);
+
+            // TODO: dar enqueue nos processos que chegaram (novos)
+            // TODO: dar enqueue nos processos que chegaram (IO)
+
+            is_done = espera(qs, numq, qios, pcbs, numpcb);
+
+            curr_time += 1;
         }
     }
 }
--- premissas.h
+++ premissas.h
@@ -5,25 +5,37 @@ typedef u16 PID;
 
 #define TIME_SLICE      4
 
-#define Premissa3       ???
-#define DISK_TIME       10
-#define TAPE_TIME       3
+
+#define DISK_TIME       3
+#define TAPE_TIME       10
 #define PRINTER_TIME    17
+
+#define DISK_NUM        1
+#define TAPE_NUM        2
+#define PRINTER_NUM     3
+
 typedef enum _IO_t {
-    IO_Disk, IO_Tape, IO_Printer
+    IO_Disk = 0, IO_Tape, IO_Printer
+    , IO_count
 } IO_t;
 
-u32 priority_from_io(IO_t io, u32 numio) {
+typedef struct _IODev {
+} IODev;
+
+u32 priority_from_io(IO_t io) {
     u32 ret = 0;
     switch (io) {
         case IO_Disk:
-            ret = numio - 1;
+            ret = 1;
             break;
         case IO_Tape:
             ret = 0;
             break;
         case IO_Printer:
             ret = 0;
+            break;
+        case IO_count:
+            assert( 0 && "unreacheable" );
             break;
     }
     return ret;
@@ -32,14 +44,16 @@ u32 priority_from_io(IO_t io, u32 numio) {
 // Gerência de Processos:
 // * Novo processo tem o PID do mais recente criado +1
 // * PCB
+typedef u16 time;
 typedef enum _PCB_Status {
     p_done = 0, p_running, p_ready, p_blocked
 } PCB_Status;
 
 typedef struct _PCB {
-    u32 priority;
-    PID ppid;
     PCB_Status status;
+    PID pid;
+    time start_time;
+    time end_time;
+    time running_time;
+    u8 priority;
 } PCB;
-// * Fila
-#define NUM_QUEUES      4
